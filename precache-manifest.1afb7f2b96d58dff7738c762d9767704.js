94a7d02c0eb8601496fc42433781bfe9a9057424
unknown <medicmtt@gmail.com>
2020-09-07 20:27:04 +0600

    Updates

--- precache-manifest.1afb7f2b96d58dff7738c762d9767704.js
+++ precache-manifest.1afb7f2b96d58dff7738c762d9767704.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e054224efac0fb63cfa8edbf23f3d80",
+    "revision": "dc03b4c87ebdfecbc107a52cae5ea289",
     "url": "/VKMA_Name/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/VKMA_Name/static/css/2.1e562f28.chunk.css"
   },
   {
-    "revision": "2c2c123c0b3e78513692",
-    "url": "/VKMA_Name/static/css/main.259adcde.chunk.css"
+    "revision": "d16b4c46a6c00ae7bfcf",
+    "url": "/VKMA_Name/static/css/main.c53521a6.chunk.css"
   },
   {
     "revision": "9f39296b2ba21358a49f",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/VKMA_Name/static/js/2.d9c1de13.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2c2c123c0b3e78513692",
+    "revision": "d16b4c46a6c00ae7bfcf",
     "url": "/VKMA_Name/static/js/main.e3edd4d5.chunk.js"
   },
   {
