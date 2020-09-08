02e8f3afb0540fcb43707a5871d8f10be0097b35
unknown <medicmtt@gmail.com>
2020-09-08 20:40:34 +0600

    Updates

--- precache-manifest.b3db6f6f673e9f59902e5d4b5009a963.js
+++ precache-manifest.b3db6f6f673e9f59902e5d4b5009a963.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ca3ba3fe6a5a67761b5289f934757b1",
+    "revision": "b9ce4eabd040246d21d1aea49c3e84b4",
     "url": "/VKMA_Name/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/VKMA_Name/static/css/2.1e562f28.chunk.css"
   },
   {
-    "revision": "dfe97c8ee01bfaf8ce9c",
+    "revision": "1b114486753d2a356e3d",
     "url": "/VKMA_Name/static/css/main.167ce813.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/VKMA_Name/static/js/2.50fe892b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dfe97c8ee01bfaf8ce9c",
-    "url": "/VKMA_Name/static/js/main.7878ce27.chunk.js"
+    "revision": "1b114486753d2a356e3d",
+    "url": "/VKMA_Name/static/js/main.f251b3ba.chunk.js"
   },
   {
     "revision": "bf2ecfc0dcd206fa6f29",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/VKMA_Name/precache-manifest.80f680b0eba7b664eb2dee901d6dc8a9.js"
+  "/VKMA_Name/precache-manifest.b3db6f6f673e9f59902e5d4b5009a963.js"
 );
 
 self.addEventListener('message', (event) => {
